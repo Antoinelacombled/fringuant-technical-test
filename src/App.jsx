2f59f4b4439d0ad3e1e9e520d26e52889f18f98c
Antoine Lacombled <antoine.j.lacombled@gmail.com>
2024-03-05 18:43:35 +0100

    feat: imported glb model test, imported three/drei, three/fiber, gsap and baffle. converted the glb character to a jsx component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+
+import {
+  RoundedBox,
+  ScrollControls,
+  Scroll,
+  SpotLight,
+  Environment,
+  OrbitControls,
+} from "@react-three/drei";
+import { Character } from "./components/Character";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <color attach="background" args={["#111111"]} />
+      <ambientLight intensity={2} />
+      <SpotLight
+        position={[40, 20, 4]}
+        angle={1.3}
+        penumbra={1}
+        castShadow
+        intensity={2}
+        shadow-bias={-0.0001}
+      />
+      <Environment background preset="warehouse" />
+      <OrbitControls />
+      <ScrollControls pages={6} damping={0.1}>
+        <Scroll>
+          <Character rotation={[0, Math.PI, 0]} />
+        </Scroll>
+        <Scroll html>
+          <h1>html in here (optional)</h1>
+          <h1 style={{ position: "absolute", top: "100vh" }}>second page</h1>
+          <h1 style={{ position: "absolute", top: "200vh" }}>third page</h1>
+        </Scroll>
+      </ScrollControls>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
